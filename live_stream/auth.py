b7a2be2b231b31b5443a511e3b62c0a24fd5e6ec
ionicc <sagarwakawaka@gmail.com>
2020-03-22 18:17:29 +0530

    Fix auth class

--- live_stream/auth.py
+++ live_stream/auth.py
@@ -5,5 +5,7 @@ class AuthHandler():
     def __init__(self, consumer_token, consumer_secret):
         self.consumer_secret = consumer_secret
         self.consumer_token = consumer_token
-        return tweepy.OAuthHandler(self.consumer_token, self.consumer_secret)
     
+    def connect(self):
+        return tweepy.OAuthHandler(self.consumer_token, self.consumer_secret)
+
