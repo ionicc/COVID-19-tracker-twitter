fc2058dd16b175cf515794d8d0c824c4d6cb8b6b
ionicc <sagarwakawaka@gmail.com>
2020-03-22 18:33:09 +0530

    Modularise and add some TODO

--- live_stream.py
+++ live_stream.py
@@ -1,10 +1,12 @@
 import tweepy
-
+from live_stream import AuthHandler
 class Streamer(tweepy.StreamListener):
 	
     def on_status(self, status):
         print(status.text)
 
 def main():
+    # Create stream instance
+    # TODO: Move Stream class to different file
     streamer = Streamer()
-    stream_listener = tweepy.Stream()
+    stream_listener = tweepy.Stream(AuthHandler())
